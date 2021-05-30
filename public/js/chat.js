496e9f509262a0822e1c9b929608f2017fbc6117
RobPerez155 <robert.perez.psirho@gmail.com>
2021-05-30 15:33:01 -0400

    Events testing - Complete

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,11 @@
 // Here we will connect to the server using websockets
-io()
+const socket = io() // this allows us to send/receive events from both the server and the client
+
+socket.on('countUpdated', (count) => {   // on -> On event do ...
+  console.log(`The Count has been updated to ${count}`)
+})
+
+document.querySelector('#increment').addEventListener('click', () => {
+  console.log('Clicked')
+  socket.emit('increment') // Sending info to the server
+})
--- src/index.js
+++ src/index.js
@@ -12,9 +12,23 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+let count = 0
+
+// Here is what's happening between index.js(server) and chat.js(client)
+  // server (emit) -> client (receive) - countUpdated
+  // client (emit) -> server (receive) - increment
+
+io.on('connection', (socket) => { // here we are listening for a specific event to occur and socket is an obj
   console.log('New WebSocket Connection')
-}) // here we are listening for a specific event to occur - ('name of event', (func to run) => {})
+
+  socket.emit('countUpdated', count)   // emit sends an event from the server to the client, anything past the first argument, which is our event name, is going to be available from the callback func on the client
+
+  socket.on('increment', () => {
+    count++
+    // socket.emit('countUpdated', count) ---> This updates only one connection
+    io.emit('countUpdated', count) // This updates all connections
+  })
+}) 
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}!`)
