980d5af64fc109b1340b18f015da2e679205fc9e
RobPerez155 <robert.perez.psirho@gmail.com>
2021-06-08 15:05:04 -0400

    Acknowledgements and Badwords - Complete

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,9 @@
 // Here we will connect to the server using websockets
 const socket = io() // this allows us to send/receive events from both the server and the client
 
+// server (emit) -> client (receive) --acknowledgement--> server
+// client (emit) -> server (receive) --acknowledgement--> client
+
 socket.on('message', (message) => {
   console.log(message)
 })
@@ -10,10 +13,17 @@ document.querySelector('#message-form').addEventListener('submit', (event) => {
 
   const message = event.target.elements.message.value
 
-  socket.emit('sendMessage', message)
+  socket.emit('sendMessage', message, (error) => { // The acknowledgement will always be the last func to run, we can add a variable name to utilize the data sent from the server.
+    if (error) {
+      return console.log(error)
+    }
+
+    console.log('Message delivered', dataSentBackFromServer)
+  })
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
+
   if (!navigator.geolocation) { // Outdated browsers may not support geolocation
     return alert('Geolocation is not supported by your browser.')
   }
@@ -23,6 +33,8 @@ document.querySelector('#send-location').addEventListener('click', () => {
     socket.emit('sendLocation', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
+    }, (sent) => {
+      console.log('Location Sent')
     })
   })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express() // This generates a new express application
 const server = http.createServer(app)
@@ -20,14 +21,24 @@ io.on('connection', (socket) => { // here we are listening for a specific event 
   console.log('New WebSocket Connection')
 
   socket.emit('message', 'Welcome HomieDuck!') // socket Sends message to current user only
+  
   socket.broadcast.emit('message', 'A new homieduck has joined!') // broadcast Sends message to everyone except current user
 
-  socket.on('sendMessage', (message) => {
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter()
+
+    if (filter.isProfane(message)) { // This scans the message for profanity before allowing it to be emitted
+      return callback('Profanity is not allowed!')
+    }
+    //else {
     io.emit('message', message) // io Sends a message to everyone
+    callback('Data from server') // This will acknowledge the event was successful, we can also send back data to the client from here 
+    //}
   })
 
-  socket.on('sendLocation', (coords) => {
+  socket.on('sendLocation', (coords, callback) => {
     io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`) // This will give us a clickable link to the location.
+    return callback()
   })
 
   socket.on('disconnect', () => {
